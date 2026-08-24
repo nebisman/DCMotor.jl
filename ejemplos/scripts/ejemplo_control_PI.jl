23d7b0021289c161ebf4a29bc1ed0d632825e3d2
nebisman <lbermeoc@unal.edu.co>
2026-08-24 09:32:59 -0500

    ejemplo PI modificado y corregido

--- ejemplos/scripts/ejemplo_control_PI.jl
+++ ejemplos/scripts/ejemplo_control_PI.jl
@@ -22,16 +22,21 @@ T= ωn^2/(s^2 + 2*ζ*ωn*s + ωn^2)
 set_pid(sys;  kp=Kp, ki=Ki, kd=0, beta=0, output=:speed, Tf=0.005)
 result = step_closed(sys; r0 = 00, r1 =400,  t0 = 1.5, t1 =2);
 stepinfo(result,T)
-# calculo de las constantes
-set_reference(sys, 6)
+
+## Ahora diseñamos un PI para velocidad con Loopshaping, 
+
 
 ωgc = 15
-Gd=G*delay(0.02)
+# Note que incluimos el retardo del muestreo digital, por lo cual se requiere 
+# el paquete completo ControlSystems
+using ControlSystems
+Gd = G*delay(0.02)
 C, Kp, Ki, fig, CF = loopshapingPI(Gd, ωgc; rl=1,  phasemargin=45, form=:parallel)
 
+T1 = feedback(C*G, 1)
 
-T1 = feedback(C*Gd, 1)
-# exoerimentos
+# y lo probamos
 set_pid(sys;  kp=Kp, ki=Ki, kd=0, beta=1, output=:speed, deadzone=0)
-result = step_closed(sys; r0 = 0, r1 = 400,  t0 = .5, t1 =1.5); stepinfo_exp(result;T=T1)
-#stairs_closed(sys, stairs=[180, 270, 360, 450,540,620] )
+result = step_closed(sys; r0 = 0, r1 = 400,  t0 = .5, t1 =1.5); 
+stepinfo(result,T1)
+
